fff40781a928497e2ec5843e4532fca5803d8bcf
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 05:57:16 -0700

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    
+    printf("Enter 1st number:");
+    printf("Enter 2nd number");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("sum = %d",c);
+
+    return 0;
+}
+
